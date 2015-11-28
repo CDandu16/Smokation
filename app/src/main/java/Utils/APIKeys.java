5e1a5ed6760c225498d45501e3a09af3bc63e8ae
ColeHud <cmhudson11@gmail.com>
2015-11-28 00:49:47 -0500

    Removed stupid git files

---
+++ app/src/main/java/Utils/APIKeys.java
@@ -0,0 +1,9 @@
+package Utils;
+
+/**
+ * Created by Cole on 11/28/15.
+ */
+public class APIKeys {
+    public final static String Application_ID = "HF1pe0JupdAMtnhT889ELamiBl4iNkC4L3VLeBVU";
+    public final static String Client_Key = "Pz5fkheoJhrZITDSN8MaxymYQVtsnG1294ODzdl9";
+}
