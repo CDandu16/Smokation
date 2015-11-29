150ca2a37b3281681a90ba57c6e3f50b41ef6920
Chaitu <chaitudandu@gmail.com>
2015-11-29 13:55:33 -0500

    GeoFencing kind of works. Just need to use parse to pull the values. Also user must click butto to enable notifications. Will try to fix it soon.

---
+++ app/src/main/java/com/example/chaitudandu/smokation/Constants.java
@@ -0,0 +1,44 @@
+package com.example.chaitudandu.smokation;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.HashMap;
+
+/**
+ * Created by Chaitu Dandu on 11/29/2015.
+ */
+public class Constants {
+    private Constants() {
+    }
+
+    public static final String PACKAGE_NAME = "com.google.android.gms.location.Geofence";
+
+    public static final String SHARED_PREFERENCES_NAME = PACKAGE_NAME + ".SHARED_PREFERENCES_NAME";
+
+    public static final String GEOFENCES_ADDED_KEY = PACKAGE_NAME + ".GEOFENCES_ADDED_KEY";
+
+    /**
+     * Used to set an expiration time for a geofence. After this amount of time Location Services
+     * stops tracking the geofence.
+     */
+    public static final long GEOFENCE_EXPIRATION_IN_HOURS = 12;
+
+    /**
+     * For this sample, geofences expire after twelve hours.
+     */
+    public static final long GEOFENCE_EXPIRATION_IN_MILLISECONDS =
+            GEOFENCE_EXPIRATION_IN_HOURS * 60 * 60 * 1000;
+    public static final float GEOFENCE_RADIUS_IN_METERS = 1609; // 1 mile, 1.6 km
+
+    /**
+     * Map for storing information about airports in the San Francisco bay area.
+     */
+    public static final HashMap<String, LatLng> BAY_AREA_LANDMARKS = new HashMap<String, LatLng>();
+    static {
+        // house
+        BAY_AREA_LANDMARKS.put("SFO", new LatLng(42.575452, -83.1394288));
+
+        // Googleplex.
+        BAY_AREA_LANDMARKS.put("GOOGLE", new LatLng(37.422611,-122.0840577));
+    }
+}
